58852321372bc3ea90731499d2bc4d7eee7719ec
Mithun K. <mail2mithunkarthick@gmail.com>
2026-02-23 18:51:03 -0500

    fourth update

--- js05.js
+++ js05.js
@@ -14,7 +14,7 @@ window.addEventListener("load", createLightbox);
 
 function createLightbox() {
    // Lightbox Container
-   let lightBox = document.getElementById("lightbox");
+   let lightBox = document.getElementById("lightBox");
 
    // Parts of the lightbox
    let lbTitle = document.createElement("h1");
@@ -23,163 +23,97 @@ function createLightbox() {
    let lbNext = document.createElement("div");
    let lbPlay = document.createElement("div");
    let lbImages = document.createElement("div");
-}
 
-// Design the lightbox title
-lightBox.appendChild(lbTitle);
-lbTitle.id = "lbTitle";
-lbTitle.textContent = lightboxTitle;
+   // Design the lightbox title
+   lightBox.appendChild(lbTitle);
+   lbTitle.id = "lbTitle";
+   lbTitle.textContent = lightboxTitle;
 
-// Design the lightbox slide counter
-lightBox.appendChild(lbCounter);
-lbCounter.id = "lbCounter";
-let currentImg = 1;
-lbCounter.textContent = currentImg + " / " + imgCount;
-
-// Design the lightbox previous slide button
-lightBox.appendChild(lbPrev);
-lbPrev.id = "lbPrev";
-lbPrev.innerHTML = "&#9664;";
-
-// Design the lightbox next slide button 
-lightBox.appendChild(lbNext);
-lbNext.id = "lbNext";
-lbNext.innerHTML = "&#9654;";
-
-// Design the lightbox Play-Pause button
-lightBox.appendChild(lbPlay);
-lbPlay.id = "lbPlay";
-lbPlay.innerHTML = "&#9199;";
-
-// Design the lightbox images container
-lightBox.appendChild(lbImages);
-lbImages.id = "lbImages";
-// Add images from the imgFiles array to the container
-for (let i = 0; i < imgFiles.length; i++) {
-   let image = document.createElement("img");
-   image.src = imgFiles[i];
-   image.alt = imgCaptions[i];
-   lbImages.appendChild(image);
-}
-
-// Function to move forward through the image list
-function ShowNext() {
-   lbImages.appendChild(lbImages.firstElementChild);
-   (currentImg < imgCount) ? currentImg++ : currentImg = 1;
+   // Design the lightbox slide counter
+   lightBox.appendChild(lbCounter);
+   lbCounter.id = "lbCounter";
+   let currentImg = 1;
    lbCounter.textContent = currentImg + " / " + imgCount;
+
+   // Design the lightbox previous slide button
+   lightBox.appendChild(lbPrev);
+   lbPrev.id = "lbPrev";
+   lbPrev.innerHTML = "&#9664;";
+   lbPrev.onclick = ShowPrev;
+
+   // Design the lightbox next slide button 
+   lightBox.appendChild(lbNext);
+   lbNext.id = "lbNext";
+   lbNext.innerHTML = "&#9654;";
+   lbNext.onclick = ShowNext;
+
+   // Design the lightbox Play-Pause button
+   lightBox.appendChild(lbPlay);
+   lbPlay.id = "lbPlay";
+   lbPlay.innerHTML = "&#9199;";
+   let timeID;
+   lbPlay.onclick = function() {
+      if (timeID) {
+         // Stop the slideshow
+         window.clearInterval(timeID);
+         timeID = undefined;
+      } else {
+         // Start the slideshow
+         ShowNext();
+         timeID = window.setInterval(ShowNext, 1500);
+      }
    }
 
-   
-
-
-function setupGallery() {
-   let imageCount = imgFiles.length;
-   let galleryBox = document.getElementById("gallery");
-   let currentSlide = 1;
-   let runShow = true;
-   let showRunning;
-   
-   let galleryTitle = document.createElement("h1");
-   galleryTitle.id = "galleryTitle";
-   galleryTitle.textContent = slidesTitle;
-   galleryBox.appendChild(galleryTitle);
-   
-   let slideCounter = document.createElement("div");
-   slideCounter.id = "slideCounter";
-   slideCounter.textContent = currentSlide + "/" + imageCount;
-   galleryBox.appendChild(slideCounter);
-   
-   let leftBox = document.createElement("div");
-   leftBox.id = "leftBox";
-   leftBox.innerHTML = "&#9664;";
-   leftBox.onclick = moveToLeft;   
-   galleryBox.appendChild(leftBox);
-   
-   let rightBox = document.createElement("div");
-   rightBox.id = "rightBox";
-   rightBox.innerHTML = "&#9654;";  
-   rightBox.onclick = moveToRight;   
-   galleryBox.appendChild(rightBox);
-   
-   let playPause = document.createElement("div");
-   playPause.id = "playPause";
-   playPause.innerHTML = "&#9199;";
-   playPause.onclick = startStopShow;
-   galleryBox.appendChild(playPause);
-   
-   let slideBox = document.createElement("div");
-   slideBox.id = "slideBox";
-   galleryBox.appendChild(slideBox);
-   
-   
-   for (let i = 0; i < imageCount; i++) {
+   // Design the lightbox images container
+   lightBox.appendChild(lbImages);
+   lbImages.id = "lbImages";
+   // Add images from the imgFiles array to the container
+   for (let i = 0; i < imgFiles.length; i++) {
       let image = document.createElement("img");
       image.src = imgFiles[i];
       image.alt = imgCaptions[i];
-      image.onclick = createModal;
-      slideBox.appendChild(image);
+      image.onclick = createOverlay;
+      lbImages.appendChild(image);
    }
+   // Function to move forward through the image list
+   function ShowNext() {
+      lbImages.appendChild(lbImages.firstElementChild);
+      (currentImg < imgCount) ? currentImg++ : currentImg = 1;
+      lbCounter.textContent = currentImg + " / " + imgCount;
+      }
    
+   // Function to move backward through the image list
+   function ShowPrev() {
+      lbImages.insertBefore(lbImages.lastElementChild, lbImages.firstElementChild);
+      (currentImg > 1) ? currentImg-- : currentImg = imgCount;
+      lbCounter.textContent = currentImg + " / " + imgCount;
+   }
+   function createOverlay() {
+      let overlay = document.createElement("div");
+      overlay.id = "lbOverlay";
 
-   
-   
-   function moveToRight() {
-      let firstImage = slideBox.firstElementChild.cloneNode("true");
-      firstImage.onclick = createModal;
-      slideBox.appendChild(firstImage);
-      slideBox.removeChild(slideBox.firstElementChild);
-      currentSlide++;
-      if (currentSlide > imageCount) {
-         currentSlide = 1;
-      }
-      slideCounter.textContent = currentSlide + " / " + imageCount;
-   }
-   
-   function moveToLeft() {
-      let lastImage = slideBox.lastElementChild.cloneNode("true");
-      lastImage.onclick = createModal;
-      slideBox.removeChild(slideBox.lastElementChild);
-      slideBox.insertBefore(lastImage, slideBox.firstElementChild);
-      currentSlide--;
-      if (currentSlide === 0) {
-         currentSlide = imageCount;
-      }
-      slideCounter.textContent = currentSlide + " / " + imageCount;      
-   }  
-   
-   function startStopShow() {
-      if (runShow) {
-         showRunning = window.setInterval(moveToRight, 2000);
-         runShow = false;
-      } else {
-         window.clearInterval(showRunning);
-         runShow = true;
-      }
-   }
-   
-   function createModal() {
-      let modalWindow = document.createElement("div");
-      modalWindow.id = "activeModal";
+      // Add the figure box to the overlay
       let figureBox = document.createElement("figure");
-      modalWindow.appendChild(figureBox);
-      
-      let modalImage = this.cloneNode("true");
-      figureBox.appendChild(modalImage);
-      
-      let figureCaption = document.createElement("figcaption");
-      figureCaption.textContent = modalImage.alt;
-      figureBox.appendChild(figureCaption);
-      
+      overlay.appendChild(figureBox);
+
+      // Add the image to the figure box
+      let overlayImage = this.cloneNode("true");
+      figureBox.appendChild(overlayImage);
+
+      // Add the caption to the figure box
+      let overlayCaption = document.createElement("figcaption");
+      overlayCaption.textContent = this.alt;
+      figureBox.appendChild(overlayCaption);
+
+      // Add a close button to the overlay
       let closeBox = document.createElement("div");
-      closeBox.id = "modalClose";
+      closeBox.id = "lbOverlayClose";
       closeBox.innerHTML = "&times;";
       closeBox.onclick = function() {
-         document.body.removeChild(modalWindow);
+         document.body.removeChild(overlay);
       }
-      
-      modalWindow.appendChild(closeBox);
-      
-      document.body.appendChild(modalWindow);
+      overlay.appendChild(closeBox);
+
+      document.body.appendChild(overlay);
    }
-   
 }
